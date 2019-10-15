b1b9625ffc79fc0b5055014d7edbf72223234e3d
aaingyunii <dlsrbs98@gmail.com>
2019-10-15 17:38:09 +0900

    Revised
    revised the button
    and need to update setting part_Ingyun

---
+++ Munmo/sample/src/main/java/com/example/munmo/sample/fragment/DeleteActionFragment.java
@@ -0,0 +1,56 @@
+package com.example.munmo.sample.fragment;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.munmo.floatingview.R;
+import com.example.munmo.sample.service.CustomFloatingViewService;
+
+
+/**
+ * Fragment that removes the FloatingView sample service.。
+ */
+public class DeleteActionFragment extends Fragment {
+
+    /**
+     * Generate DeleteActionFragment。
+     *
+     * @return DeleteActionFragment
+     */
+    public static DeleteActionFragment newInstance() {
+        final DeleteActionFragment fragment = new DeleteActionFragment();
+        return fragment;
+    }
+
+    /**
+     * construct
+     */
+    public DeleteActionFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.fragment_delete_action, container, false);
+        // delete button
+        final View clearFloatingButton = rootView.findViewById(R.id.clearDemo);
+        clearFloatingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Easy way to delete a service
+                final Activity activity = getActivity();
+                activity.stopService(new Intent(activity, CustomFloatingViewService.class));
+            }
+        });
+        return rootView;
+    }
+}
